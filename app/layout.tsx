d5546680d6ce69c8887f0ec47ab6803919e0f7f0
Fabian Keller <fabian.keller@welytics.ai>
2024-07-19 11:18:37 +0200

    feat: added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}
 
-
+      <SpeedInsights />
       <script async src="https://gc.datafakery.io/tracker.js" data-ackee-server="https://gc.datafakery.io" data-ackee-domain-id="bfa7dff0-205d-4864-a389-9047cdc9e484"></script>
       </body>
     </html>
