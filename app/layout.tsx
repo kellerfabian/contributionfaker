14b354e36b80982823898da047436b10694fa5aa
Fabian Keller <fabian.keller@welytics.ai>
2024-08-15 16:40:52 +0200

    chore: cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
+        {children}
 
-      <SpeedInsights />
-      <Analytics />
-      <script async src="https://gc.datafakery.io/tracker.js" data-ackee-server="https://gc.datafakery.io" data-ackee-domain-id="bfa7dff0-205d-4864-a389-9047cdc9e484"></script>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
